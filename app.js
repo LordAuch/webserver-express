21ddf29d72d64921d8be4ccca1a87d4dd89decb1
Lord Auch <ayisael@gmail.com>
2019-07-12 15:25:13 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+const http = require('http');
+
+http.createServer( (req, resp)=>{
+
+  resp.writeHead(200, { 'Content-Type': 'application/json'});
+
+  let salida = {
+    nombre: 'Edher',
+    edad: 19,
+    url: req.url
+  }
+
+  resp.write(JSON.stringify(salida));
+
+  // resp.write('Hola mundo');
+  resp.end();
+
+})
+.listen(8080);
+
+console.log('Escuchando el puerto 8080');
